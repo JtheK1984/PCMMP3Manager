8e77217f4adcb45da9930ca631464fbc37c7fdf7
JtheK1984 <Jens.Henske@outlook.com>
2024-08-12 08:58:35 +0200

    Version 1.6

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,6 +1,6 @@
 echo "Kopiere Datei ins Setupverzeichnis"
 copy /y /v Win32\Release\PCMMp3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v Win32\Release\PCMMp3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
+copy /y /v Win64\Release\PCMMp3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
 
 copy /y /v Win64\Release\PCMMp3Manager.DE "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
 copy /y /v Win64\Release\PCMMp3Manager.EN "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
