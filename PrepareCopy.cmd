535655b9795cd456ca67bc77880e362bf8af0f34
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-07-28 13:14:45 +0200

    Version 1.0

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,15 +1,16 @@
-echo "Kopiere Datei ins Setupverzeichnis"
-copy /y /v Win32\Release\PCMMp3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v Win64\Release\PCMMp3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
+echo "Kopiere Datei ins Setupverzeichnis 32-Bit"
+copy /y /v Win32\Release\PCMMP3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
+copy /y /v Win32\Release\PCMMP3Manager.DE "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
+copy /y /v Win32\Release\PCMMP3Manager.EN "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
+echo "Kopiere Datei ins Setupverzeichnis 64-Bit"
+copy /y /v Win64\Release\PCMMP3Manager.exe "e:\Inno\Setupfiles\Programme\PCMMP3Manager_x64"
+copy /y /v Win64\Release\PCMMP3Manager.DE "e:\Inno\Setupfiles\Programme\PCMMP3Manager_x64"
+copy /y /v Win64\Release\PCMMP3Manager.EN "e:\Inno\Setupfiles\Programme\PCMMP3Manager_x64"
 
-copy /y /v Win64\Release\PCMMp3Manager.DE "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v Win64\Release\PCMMp3Manager.EN "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v Win64\Release\PCMMp3Manager.DE "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
-copy /y /v Win64\Release\PCMMp3Manager.EN "e:\Inno\Setupfiles\Programme\PCMMp3Manager_x64"
-
-copy /y /v "PCMMp3Manager.docx" "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v "PCMMp3Manager.pdf" "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
-copy /y /v "PCMMp3Manager.htm" "e:\Inno\Setupfiles\Programme\PCMMp3Manager"
+echo "Kopiere Doku ins Setupverzeichnis"
+copy /y /v PCMMP3Manager.docx "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
+copy /y /v PCMMP3Manager.pdf "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
+copy /y /v PCMMP3Manager.htm "e:\Inno\Setupfiles\Programme\PCMMP3Manager"
 
 echo "Demolizenz erstellen"
-call "E:\Inno\Setupfiles\Programme\PCMLizenzgenerator\PCMLizenzgenerator.exe" /PCM-Mp3Manager /1.0 /E:\Inno\Setupfiles\Programme\PCMMp3Manager
+call "E:\Inno\Setupfiles\Programme\PCMLizenzgenerator\PCMLizenzgenerator.exe" /PCM-MP3Manager /1.0 /E:\Inno\Setupfiles\Programme\PCMMP3Manager
